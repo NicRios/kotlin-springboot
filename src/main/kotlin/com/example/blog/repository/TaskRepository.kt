5632576c34fc83af7485e674710b116fe1552bc9
NicRios <n.rios.1997@gmail.com>
2020-07-21 06:24:24 -0700

    first commit

---
+++ src/main/kotlin/com/example/blog/repository/TaskRepository.kt
@@ -0,0 +1,10 @@
+package com.example.blog.repository
+
+import com.example.blog.model.Task
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import javax.transaction.Transactional
+
+@Repository
+@Transactional(Transactional.TxType.MANDATORY)
+interface TaskRepository : JpaRepository<Task, Long>
